1361c95727711cf4a56544e8377b4226789b84f5
Dav1dH <david.e.harutyunyan@gmail.com>
2021-02-19 17:36:27 +0400

    Bug fixed

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -7,26 +7,28 @@ import { User } from '../auth/user.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
+
+    query.where('task.userId = :userId', { userId: user.id });
 
     if (status) {
       query.andWhere('task.status = :status', { status });
     }
 
     if (search) {
-      query.andWhere('(task.title LIKE :search OR task.description LIKE :search)', { search: `%${search}%` });
+      query.andWhere(
+        '(task.title LIKE :search OR task.description LIKE :search)',
+        { search: `%${search}%` },
+      );
     }
 
     const tasks = await query.getMany();
     return tasks;
   }
 
-  async createTask(
-    createTaskDto: CreateTaskDto,
-    user: User,
-  ): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
 
     const task = new Task();
